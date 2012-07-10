f2bdd26d1f4644edb141ed50406722178d2ba415
Peter Hutterer <peter.hutterer@who-t.net>
2012-07-10 14:21:35 +1000

    Fix indentation in input-driver-test.cpp
    Indentation came from copy/paste, was out by a few spaces.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- tests/input-driver-test.cpp
+++ tests/input-driver-test.cpp
@@ -8,60 +8,60 @@
 #include "input-driver-test.h"
 
 void InputDriverTest::StartServer() {
-        server.SetOption("-logfile",log_file);
-        server.Start();
-        server.WaitForConnections();
-        xorg::testing::Test::SetDisplayString(server.GetDisplayString());
+    server.SetOption("-logfile",log_file);
+    server.Start();
+    server.WaitForConnections();
+    xorg::testing::Test::SetDisplayString(server.GetDisplayString());
 
-        ASSERT_NO_FATAL_FAILURE(xorg::testing::Test::SetUp());
+    ASSERT_NO_FATAL_FAILURE(xorg::testing::Test::SetUp());
 
-        int event_start;
-        int error_start;
+    int event_start;
+    int error_start;
 
-        ASSERT_TRUE(XQueryExtension(Display(), "XInputExtension", &xi2_opcode,
-                                    &event_start, &error_start));
+    ASSERT_TRUE(XQueryExtension(Display(), "XInputExtension", &xi2_opcode,
+                &event_start, &error_start));
 
-        int major = 2;
-        int minor = 0;
+    int major = 2;
+    int minor = 0;
 
-        ASSERT_EQ(Success, XIQueryVersion(Display(), &major, &minor));
-    }
+    ASSERT_EQ(Success, XIQueryVersion(Display(), &major, &minor));
+}
 
 void InputDriverTest::SetUp() {
-        failed = false;
+    failed = false;
 
-        testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
-        listeners.Append(this);
+    testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
+    listeners.Append(this);
 
-        std::stringstream s;
-        s << "/tmp/Xorg-" << GetParam() << ".log";
-        log_file = s.str();
+    std::stringstream s;
+    s << "/tmp/Xorg-" << GetParam() << ".log";
+    log_file = s.str();
 
-        s.str(std::string());
-        s << "/tmp/" << GetParam() << ".conf";
-        config_file = s.str();
+    s.str(std::string());
+    s << "/tmp/" << GetParam() << ".conf";
+    config_file = s.str();
 
-        config.AddInputSection(GetParam());
-        config.WriteConfig(config_file);
-        server.SetOption("-config", config_file);
-        StartServer();
-    }
+    config.AddInputSection(GetParam());
+    config.WriteConfig(config_file);
+    server.SetOption("-config", config_file);
+    StartServer();
+}
 
 void InputDriverTest::TearDown() {
-        if (server.Pid() != -1)
-            if (!server.Terminate())
-                server.Kill();
+    if (server.Pid() != -1)
+        if (!server.Terminate())
+            server.Kill();
 
-        if (!failed) {
-            if (config_file.size())
-                unlink(config_file.c_str());
+    if (!failed) {
+        if (config_file.size())
+            unlink(config_file.c_str());
 
-            if (log_file.size())
-                unlink(log_file.c_str());
-        }
+        if (log_file.size())
+            unlink(log_file.c_str());
+    }
 
-        testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
-        listeners.Release(this);
+    testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
+    listeners.Release(this);
 }
 
 void InputDriverTest::OnTestPartResult(const ::testing::TestPartResult &test_part_result) {
