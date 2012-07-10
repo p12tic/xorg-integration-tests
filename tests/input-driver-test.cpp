bd44f252f180a012c5c6cadfcbb5d3e4e7a3f2ab
Peter Hutterer <peter.hutterer@who-t.net>
2012-07-10 11:41:40 +1000

    Swith test to use a common class implementation
    Most tests will likely be very similar, let's not re-implement the class in
    each test but use one where it makes sense.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

---
+++ tests/input-driver-test.cpp
@@ -0,0 +1,99 @@
+#include <stdexcept>
+#include <fstream>
+#include <xorg/gtest/xorg-gtest.h>
+
+#include "input-driver-test.h"
+
+void InputDriverTest::WriteConfig(const char *param) {
+        std::stringstream s;
+        s << "/tmp/" << param << ".conf";
+        config_file = s.str();
+
+        std::ofstream conffile(config_file.c_str());
+        conffile.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+
+        std::string driver(param);
+
+        conffile << ""
+"            Section \"ServerFlags\"\n"
+"                Option \"Log\" \"flush\"\n"
+"            EndSection\n"
+"\n"
+"            Section \"ServerLayout\"\n"
+"                Identifier \"Dummy layout\"\n"
+"                Screen 0 \"Dummy screen\" 0 0\n"
+"                Option \"AutoAddDevices\" \"off\"\n"
+"                InputDevice \"--device--\"\n"
+"            EndSection\n"
+"\n"
+"            Section \"Screen\"\n"
+"                Identifier \"Dummy screen\"\n"
+"                Device \"Dummy video device\"\n"
+"            EndSection\n"
+"\n"
+"            Section \"Device\"\n"
+"                Identifier \"Dummy video device\"\n"
+"                Driver \"dummy\"\n"
+"            EndSection\n";
+        server.SetOption("-config", config_file);
+}
+
+void InputDriverTest::AddInputSection(std::string driver,
+                                      std::string identifier,
+                                      std::string options) {
+        std::ofstream conffile(config_file.c_str(), std::ios_base::app);
+        conffile.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+
+        conffile << ""
+"            Section \"InputDevice\"\n"
+"                Identifier \"" << identifier << "\"\n"
+"                Driver \"" << driver << "\"\n"
+                 << options <<
+"            EndSection\n";
+        server.SetOption("-config", config_file);
+}
+
+void InputDriverTest::StartServer() {
+        server.SetOption("-logfile",log_file);
+        server.Start();
+        server.WaitForConnections();
+        xorg::testing::Test::SetDisplayString(server.GetDisplayString());
+
+        ASSERT_NO_FATAL_FAILURE(xorg::testing::Test::SetUp());
+    }
+
+void InputDriverTest::SetUp() {
+        failed = false;
+
+        testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
+        listeners.Append(this);
+
+        std::stringstream s;
+        s << "/tmp/Xorg-" << GetParam() << ".log";
+        log_file = s.str();
+
+        WriteConfig(GetParam());
+        AddInputSection(GetParam());
+        StartServer();
+    }
+
+void InputDriverTest::TearDown() {
+        if (server.Pid() != -1)
+            if (!server.Terminate())
+                server.Kill();
+
+        if (!failed) {
+            if (config_file.size())
+                unlink(config_file.c_str());
+
+            if (log_file.size())
+                unlink(log_file.c_str());
+        }
+
+        testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
+        listeners.Release(this);
+}
+
+void InputDriverTest::OnTestPartResult(const ::testing::TestPartResult &test_part_result) {
+    failed = test_part_result.failed();
+}
---
+++ tests/input-driver-test.h
@@ -0,0 +1,52 @@
+#ifndef _INPUT_DRIVER_TEST_H_
+#define _INPUT_DRIVER_TEST_H_
+
+#include <xorg/gtest/xorg-gtest.h>
+
+/**
+ * A test fixture for testing some input drivers
+ *
+ */
+class InputDriverTest : public xorg::testing::Test,
+                        public ::testing::EmptyTestEventListener,
+                        public ::testing::WithParamInterface<const char*> {
+protected:
+    /**
+     * Write a temporary config file prefixed with the given argument,
+     * resulting in a file name of /tmp/prefix.conf.
+     *
+     * On test success, the config file will be deleted.
+     *
+     * @param prefix The config file name prefix
+     */
+    virtual void WriteConfig(const char *prefix);
+
+
+    /**
+     * Add a Section InputDevice to the config file.
+     *
+     * @param identifier Device identifier, or "--device--" if left out
+     * @param driver The driver used for this device
+     * @param options "Option foo " directives.
+     */
+    virtual void AddInputSection(std::string driver,
+                                 std::string identifier = "--device--",
+                                 std::string options = "");
+
+    /**
+     * Sets ::failed to true if this test failed.
+     */
+    virtual void OnTestPartResult(const ::testing::TestPartResult &test_part_result);
+
+    virtual void StartServer();
+    virtual void SetUp();
+    virtual void TearDown();
+
+    std::string config_file;
+    std::string log_file;
+    xorg::testing::XServer server;
+    bool failed;
+
+};
+
+#endif
