1909db01f619dabfa7576feca48e2e1220c476a4
Peter Hutterer <peter.hutterer@who-t.net>
2012-07-10 13:51:13 +1000

    Use new xorg.conf class from input driver test
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- tests/input-driver-test.cpp
+++ tests/input-driver-test.cpp
@@ -7,53 +7,6 @@
 
 #include "input-driver-test.h"
 
-void InputDriverTest::WriteConfig(const std::string &param) {
-        std::stringstream s;
-        s << "/tmp/" << param << ".conf";
-        config_file = s.str();
-
-        std::ofstream conffile(config_file.c_str());
-        conffile.exceptions(std::ofstream::failbit | std::ofstream::badbit);
-
-        conffile << ""
-"            Section \"ServerFlags\"\n"
-"                Option \"Log\" \"flush\"\n"
-"            EndSection\n"
-"\n"
-"            Section \"ServerLayout\"\n"
-"                Identifier \"Dummy layout\"\n"
-"                Screen 0 \"Dummy screen\" 0 0\n"
-"                Option \"AutoAddDevices\" \"off\"\n"
-"                InputDevice \"--device--\"\n"
-"            EndSection\n"
-"\n"
-"            Section \"Screen\"\n"
-"                Identifier \"Dummy screen\"\n"
-"                Device \"Dummy video device\"\n"
-"            EndSection\n"
-"\n"
-"            Section \"Device\"\n"
-"                Identifier \"Dummy video device\"\n"
-"                Driver \"dummy\"\n"
-"            EndSection\n";
-        server.SetOption("-config", config_file);
-}
-
-void InputDriverTest::AddInputSection(std::string driver,
-                                      std::string identifier,
-                                      std::string options) {
-        std::ofstream conffile(config_file.c_str(), std::ios_base::app);
-        conffile.exceptions(std::ofstream::failbit | std::ofstream::badbit);
-
-        conffile << ""
-"            Section \"InputDevice\"\n"
-"                Identifier \"" << identifier << "\"\n"
-"                Driver \"" << driver << "\"\n"
-                 << options <<
-"            EndSection\n";
-        server.SetOption("-config", config_file);
-}
-
 void InputDriverTest::StartServer() {
         server.SetOption("-logfile",log_file);
         server.Start();
@@ -84,8 +37,13 @@ void InputDriverTest::SetUp() {
         s << "/tmp/Xorg-" << GetParam() << ".log";
         log_file = s.str();
 
-        WriteConfig(GetParam());
-        AddInputSection(GetParam());
+        s.str(std::string());
+        s << "/tmp/" << GetParam() << ".conf";
+        config_file = s.str();
+
+        config.AddInputSection(GetParam());
+        config.WriteConfig(config_file);
+        server.SetOption("-config", config_file);
         StartServer();
     }
 
--- tests/input-driver-test.h
+++ tests/input-driver-test.h
@@ -2,6 +2,7 @@
 #define _INPUT_DRIVER_TEST_H_
 
 #include <xorg/gtest/xorg-gtest.h>
+#include "xorg-conf.h"
 
 /**
  * A test fixture for testing some input drivers
@@ -11,28 +12,6 @@ class InputDriverTest : public xorg::testing::Test,
                         public ::testing::EmptyTestEventListener,
                         public ::testing::WithParamInterface<std::string> {
 protected:
-    /**
-     * Write a temporary config file prefixed with the given argument,
-     * resulting in a file name of /tmp/prefix.conf.
-     *
-     * On test success, the config file will be deleted.
-     *
-     * @param prefix The config file name prefix
-     */
-    virtual void WriteConfig(const std::string &prefix);
-
-
-    /**
-     * Add a Section InputDevice to the config file.
-     *
-     * @param identifier Device identifier, or "--device--" if left out
-     * @param driver The driver used for this device
-     * @param options "Option foo " directives.
-     */
-    virtual void AddInputSection(std::string driver,
-                                 std::string identifier = "--device--",
-                                 std::string options = "");
-
     /**
      * Sets ::failed to true if this test failed.
      */
@@ -48,6 +27,7 @@ protected:
     xorg::testing::XServer server;
     bool failed;
 
+    XOrgConfig config;
 };
 
 #endif
