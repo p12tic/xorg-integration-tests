93d3208f037ea212efd5b542120ca170bcf656c7
Olivier Fourdan <ofourdan@redhat.com>
2012-07-12 13:30:19 +0200

    kbd-driver: check for layout
    Generate a sequence of Q.W.E.R.T.Y key presses and match to
    actual expected keysyms for various layouts, namely "us",
    "de" and "fr" that are known to differ for that sequence.
    
    The goal is to test that the layout given to the keyboard
    driver is applied.
    
    Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>

--- tests/kbd.cpp
+++ tests/kbd.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <map>
 #include <xorg/gtest/xorg-gtest.h>
 #include <linux/input.h>
 
@@ -10,6 +11,10 @@
 
 #include "input-driver-test.h"
 
+typedef std::pair <int, KeySym> Key_Pair;
+typedef std::multimap<std::string, Key_Pair> Keys_Map;
+typedef Keys_Map::iterator keys_mapIter;
+
 class KeyboardDriverTest : public InputDriverTest {
     virtual void SetUp() {
 
@@ -18,6 +23,29 @@ class KeyboardDriverTest : public InputDriverTest {
                     RECORDINGS_DIR "keyboards/AT Translated Set 2 Keyboard.desc"
                     )
                 );
+
+        // Define a map of pair to hold each key/keysym per layout
+        // US, QWERTY => qwerty
+        Keys.insert (std::pair<std::string, Key_Pair> ("us", Key_Pair (KEY_Q, XK_q)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("us", Key_Pair (KEY_W, XK_w)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("us", Key_Pair (KEY_E, XK_e)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("us", Key_Pair (KEY_R, XK_r)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("us", Key_Pair (KEY_T, XK_t)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("us", Key_Pair (KEY_Y, XK_y)));
+        // German, QWERTY => qwertz
+        Keys.insert (std::pair<std::string, Key_Pair> ("de", Key_Pair (KEY_Q, XK_q)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("de", Key_Pair (KEY_W, XK_w)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("de", Key_Pair (KEY_E, XK_e)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("de", Key_Pair (KEY_R, XK_r)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("de", Key_Pair (KEY_T, XK_t)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("de", Key_Pair (KEY_Y, XK_z)));
+        // French, QWERTY => azerty
+        Keys.insert (std::pair<std::string, Key_Pair> ("fr", Key_Pair (KEY_Q, XK_a)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("fr", Key_Pair (KEY_W, XK_z)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("fr", Key_Pair (KEY_E, XK_e)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("fr", Key_Pair (KEY_R, XK_r)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("fr", Key_Pair (KEY_T, XK_t)));
+        Keys.insert (std::pair<std::string, Key_Pair> ("fr", Key_Pair (KEY_Y, XK_y)));
 
         InputDriverTest::SetUp();
     }
@@ -28,16 +56,21 @@ class KeyboardDriverTest : public InputDriverTest {
 
         /* we don't use the dummy driver here, for some reason we won't get
          * key events with it */
-        config.AddInputSection("kbd", "--device--", "Option \"CoreKeyboard\" \"on\"\n");
+        config.AddInputSection("kbd", "--device--",
+                               "Option \"CoreKeyboard\" \"on\"\n"
+                               "Option \"XkbRules\"   \"xorg\"\n"
+                               "Option \"XkbModel\"   \"pc104\"\n"
+                               "Option \"XkbLayout\"  \""+ prefix + "\"\n");
         config.WriteConfig("/tmp/kbd-driver.conf");
     }
 
     protected:
     std::auto_ptr<xorg::testing::evemu::Device> dev;
+    Keys_Map Keys;
 };
 
 
-TEST_F(KeyboardDriverTest, DeviceExists)
+TEST_P(KeyboardDriverTest, DeviceExists)
 {
     std::string param;
     int ndevices;
@@ -57,28 +90,42 @@ TEST_F(KeyboardDriverTest, DeviceExists)
     XIFreeDeviceInfo(info);
 }
 
-TEST_F(KeyboardDriverTest, QWERTY)
+TEST_P(KeyboardDriverTest, KeyboardLayout)
 {
+    std::string layout = GetParam();
+
     XSelectInput(Display(), DefaultRootWindow(Display()), KeyPressMask | KeyReleaseMask);
-    /* the server takes a while to start up but the devices may not respond
+    /* the server takes a while to start up bust the devices may not respond
        to events yet. Add a noop call that just delays everything long
        enough for this test to work */
     XInternAtom(Display(), "foo", True);
     XFlush(Display());
 
-    dev->PlayOne(EV_KEY, KEY_Q, 1, 1);
-    dev->PlayOne(EV_KEY, KEY_Q, 0, 1);
+    keys_mapIter it;
+    std::pair<keys_mapIter, keys_mapIter> keyRange = Keys.equal_range(layout);
+    for (it = keyRange.first;  it != keyRange.second;  ++it)
+    {
+        Key_Pair pair = (*it).second;
 
-    XSync(Display(), False);
-    ASSERT_NE(XPending(Display()), 0);
+        dev->PlayOne(EV_KEY, pair.first, 1, 1);
+        dev->PlayOne(EV_KEY, pair.first, 0, 1);
 
-    XEvent press;
-    XNextEvent(Display(), &press);
+        XSync(Display(), False);
+        ASSERT_NE(XPending(Display()), 0) << "No event pending" << std::endl;
 
-    KeySym sym = XKeycodeToKeysym(Display(), press.xkey.keycode, 0);
-    ASSERT_EQ(sym, XK_q);
+        XEvent press;
+        XNextEvent(Display(), &press);
+
+        KeySym sym = XKeycodeToKeysym(Display(), press.xkey.keycode, 0);
+        ASSERT_EQ(pair.second, sym) << "Keysym not matching" << std::endl;
+
+        XSync(Display(), False);
+        while (XPending(Display()))
+            XNextEvent(Display(), &press);
+    }
 }
 
+INSTANTIATE_TEST_CASE_P(, KeyboardDriverTest, ::testing::Values("us", "de", "fr"));
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
