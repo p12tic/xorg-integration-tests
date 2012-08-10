fb540246dc79f841d1ccd254980b3b208d7880e4
Peter Hutterer <peter.hutterer@who-t.net>
2012-08-10 10:26:33 +1000

    xorg-conf: update documentation
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- tests/xorg-conf.h
+++ tests/xorg-conf.h
@@ -21,8 +21,8 @@
 class XOrgConfig {
 public:
     /**
-     * Create a new configuration file. This doesn't write to a file until
-     * WriteConfig() is called.
+     * Create a new configuration file. A configuration file is not written
+     * to the file system until WriteConfig() is called.
      *
      * @param path The future default path to write to, or a default path if
      * none is given
@@ -30,7 +30,8 @@ public:
     XOrgConfig(const std::string& path = "/tmp/xorg.conf");
 
     /**
-     * Write the current config to the path given.
+     * Write the current config to the path given. If no path is specified,
+     * the path used is the one either set in XOrgConfig() or by SetPath().
      *
      * @param path The target path to write to, or the stored path in the
      * object
@@ -40,13 +41,29 @@ public:
     /**
      * Remove the config previously written (if any).
      *
-     * This must be called explicitly so that the config stays around on
-     * failure.
+     * This must be called explicitly, the destructor does not remove the
+     * the config file. This is intentional, a test that fails and thus
+     * leaves the scope should leave the config file around that cause the
+     * test failure.
      */
     virtual void RemoveConfig();
 
     /**
-     * Add a Section InputDevice to the config file.
+     * Add a Section InputDevice to the config file. This section is
+     * optionally referenced from the ServerLayout written by this section,
+     * and in the form of:
+     *
+     * @code
+     * Section "InputDevice"
+     *    Identifier "--device--"
+     *    Driver "driver"
+     *    ...
+     * EndSection
+     * @endcode
+     *
+     * Options may be specified by the caller and must be a single string.
+     * It is recommened that multiple options are separated by line-breaks
+     * to improve the readability of the config file.
      *
      * @param identifier Device identifier, or "--device--" if left out
      * @param driver The driver used for this device
@@ -59,18 +76,40 @@ public:
                                  const std::string &options = "",
                                  bool reference_from_layout = true);
 
+    /**
+     * Add a Screen and Device section to the config file. The resulting
+     * config file will look like this:
+     *
+     * @code
+     * Section "Device"
+     *     Identifier "dummy"
+     *     Driver "dummy"
+     *     ...
+     * EndSection
+     *
+     * Section "Screen"
+     *     Identifier "dummy"
+     *     Device "dummy"
+     * EndSection
+     * @endcode
+     *
+     * Options may be specified by the caller and must be a single string.
+     * It is recommened that multiple options are separated by line-breaks
+     * to improve the readability of the config file.
+     */
     void AddDefaultScreenWithDriver(const std::string &driver = "dummy",
                                     const std::string &identifier = "dummy",
                                     const std::string &options = "");
 
     /**
-     * Enable or disable AutoAddDevices
+     * Enable or disable AutoAddDevices.
+     *
      * @param enabled True to enable AutoAddDevices, False to disable
      */
     void SetAutoAddDevices(bool enabled);
 
     /**
-     * @return the path of the config file.
+     * @return The path of the config file.
      */
     const std::string& GetPath();
 
