e3af2c5bb61cf36c36e495227fa9e4eebaaaf375
Peter Hutterer <peter.hutterer@who-t.net>
2012-11-07 15:12:12 +1000

    server/touch: add pointer jump test
    Moving a touch device, then a mouse device must move the pointer from the
    last position of the touch, not jump back elsewhere.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- tests/server/touch.cpp
+++ tests/server/touch.cpp
@@ -796,6 +796,64 @@ INSTANTIATE_TEST_CASE_P(, TouchEventHistoryTest,
                         ::testing::ValuesIn(generate_event_history_test_cases(WINDOW_HIERARCHY_DEPTH)));
 
 
+class TouchDeviceChangeTest : public TouchTest {
+protected:
+    virtual void SetUp() {
+        mouse = std::auto_ptr<xorg::testing::evemu::Device>(
+                new xorg::testing::evemu::Device(
+                    RECORDINGS_DIR "/mice/PIXART-USB-OPTICAL-MOUSE.desc")
+                );
+
+        TouchTest::SetUp();
+    }
+
+    virtual void SetUpConfigAndLog() {
+        config.AddInputSection("evdev", "mouse",
+                               "Option \"CorePointer\" \"on\""
+                               "Option \"AccelerationProfile\" \"-1\""
+                               "Option \"Device\" \"" + mouse->GetDeviceNode() + "\"");
+        TouchTest::SetUpConfigAndLog();
+    }
+
+    std::auto_ptr<xorg::testing::evemu::Device> mouse;
+};
+
+TEST_F(TouchDeviceChangeTest, NoCursorJumpsOnTouchToPointerSwitch)
+{
+    XORG_TESTCASE("Create a touch device and a mouse device.\n"
+                  "Move pointer to bottom right corner through the mouse\n"
+                  "Touch and release from the touch device\n"
+                  "Move pointer\n"
+                  "Expect pointer motion close to touch end coordinates\n");
+
+    ::Display *dpy = Display();
+
+    double x, y;
+    do {
+        mouse->PlayOne(EV_REL, REL_X, 10, false);
+        mouse->PlayOne(EV_REL, REL_Y, 10, true);
+        QueryPointerPosition(dpy, &x, &y);
+    } while(x < DisplayWidth(dpy, DefaultScreen(dpy)) - 1 &&
+            y < DisplayHeight(dpy, DefaultScreen(dpy))- 1);
+
+    dev->Play(RECORDINGS_DIR "tablets/N-Trig-MultiTouch.touch_1_begin.events");
+    dev->Play(RECORDINGS_DIR "tablets/N-Trig-MultiTouch.touch_1_end.events");
+
+    double touch_x, touch_y;
+
+    QueryPointerPosition(dpy, &touch_x, &touch_y);
+
+    /* check if we did move the pointer with the touch */
+    ASSERT_NE(x, touch_x);
+    ASSERT_NE(y, touch_y);
+
+    mouse->PlayOne(EV_REL, REL_X, 1, true);
+    QueryPointerPosition(dpy, &x, &y);
+
+    ASSERT_EQ(touch_x + 1, x);
+    ASSERT_EQ(touch_y, y);
+}
+
 #endif /* HAVE_XI22 */
 
 INSTANTIATE_TEST_CASE_P(, TouchTestXI2Version, ::testing::Range(0, XI_2_Minor + 1));
