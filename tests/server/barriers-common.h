d162f6fcaa04a2448b08cebc8499224c15cdc712
Peter Hutterer <peter.hutterer@who-t.net>
2012-12-11 16:57:13 +1000

    server/barriers: move BarrierDevices class into common header
    will be re-used from event tests
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- tests/server/barriers-common.h
+++ tests/server/barriers-common.h
@@ -56,4 +56,70 @@ public:
     }
 };
 
+class BarrierDevices : public XITServerInputTest {
+public:
+    std::auto_ptr<xorg::testing::evemu::Device> dev1;
+    std::auto_ptr<xorg::testing::evemu::Device> dev2;
+
+    int master_id_1;
+    int master_id_2;
+
+    virtual void SetUp() {
+        dev1 = std::auto_ptr<xorg::testing::evemu::Device>(
+                new xorg::testing::evemu::Device(
+                    RECORDINGS_DIR "mice/PIXART-USB-OPTICAL-MOUSE-HWHEEL.desc"
+                ));
+        dev2 = std::auto_ptr<xorg::testing::evemu::Device>(
+                new xorg::testing::evemu::Device(
+                    RECORDINGS_DIR "mice/PIXART-USB-OPTICAL-MOUSE-HWHEEL.desc"
+                ));
+        XITServerInputTest::SetUp();
+        ConfigureDevices();
+    }
+
+    void ConfigureDevices() {
+        ::Display *dpy = Display();
+        int device_id_2;
+
+        XIAnyHierarchyChangeInfo change;
+        change.add = (XIAddMasterInfo) {
+            .type = XIAddMaster,
+            .name = (char *) "New Master",
+            .send_core = False,
+            .enable = True
+        };
+
+        master_id_1 = VIRTUAL_CORE_POINTER_ID;
+
+        ASSERT_EQ(XIChangeHierarchy(dpy, &change, 1), Success) << "Couldn't add the new master device.";
+        ASSERT_TRUE(xorg::testing::XServer::WaitForDevice(dpy, "New Master pointer")) << "Didn't get the new master pointer device.";
+        ASSERT_TRUE(FindInputDeviceByName(dpy, "New Master pointer", &master_id_2)) << "Failed to find the new master pointer.";
+        ASSERT_TRUE(FindInputDeviceByName(dpy, "--device2--", &device_id_2)) << "Failed to find device2.";
+
+        change.attach = (XIAttachSlaveInfo) {
+            .type = XIAttachSlave,
+            .deviceid = device_id_2,
+            .new_master = master_id_2,
+        };
+        ASSERT_EQ(XIChangeHierarchy(dpy, &change, 1), Success) << "Couldn't attach device2 to the new master pointer.";
+    }
+
+    virtual void SetUpConfigAndLog() {
+        config.AddDefaultScreenWithDriver();
+        config.AddInputSection("evdev", "--device1--",
+                               "Option \"CorePointer\" \"on\""
+                               "Option \"AccelerationProfile\" \"-1\""
+                               "Option \"Device\" \"" + dev1->GetDeviceNode() + "\"");
+
+        config.AddInputSection("evdev", "--device2--",
+                               "Option \"CorePointer\" \"on\""
+                               "Option \"AccelerationProfile\" \"-1\""
+                               "Option \"Device\" \"" + dev2->GetDeviceNode() + "\"");
+
+        config.AddInputSection("kbd", "kbd-device",
+                               "Option \"CoreKeyboard\" \"on\"\n");
+        config.WriteConfig();
+    }
+};
+
 #endif
