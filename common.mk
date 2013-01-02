b749ab4ae7e53ca4759c61eeb858b48fff867686
Peter Hutterer <peter.hutterer@who-t.net>
2013-01-02 14:24:00 +1000

    Build the static XIT library only once
    Don't build it per-directory, build in once only.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

---
+++ common.mk
@@ -0,0 +1,26 @@
+include $(top_srcdir)/Makefile-xorg-gtest.am
+
+noinst_LIBRARIES = $(XORG_GTEST_BUILD_LIBS)
+
+AM_CPPFLAGS = \
+	      $(XI_CFLAGS) \
+	      $(XFIXES_CFLAGS) \
+	      $(XRANDR_CFLAGS) \
+	      $(WACOM_CFLAGS) \
+	      $(GTEST_CPPFLAGS) \
+	      $(XORG_GTEST_CPPFLAGS) \
+              -Wall \
+	      -I$(top_srcdir)/tests/common \
+	      -DTEST_ROOT_DIR=\"$(abs_top_srcdir)/tests/\" \
+	      -DRECORDINGS_DIR=\"$(abs_top_srcdir)/recordings/\"
+
+AM_CXXFLAGS = $(GTEST_CXXFLAGS) $(XORG_GTEST_CXXFLAGS)
+
+GTEST_LDADDS = \
+	       $(XI_LIBS) \
+	       $(XRANDR_LIBS) \
+	       $(XORG_GTEST_LIBS) \
+	       $(EVEMU_LIBS) \
+	       $(XORG_GTEST_MAIN_LIBS)
+
+XIT_LIBS=$(top_builddir)/tests/common/libxit.a
