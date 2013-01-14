b727ed67b10b5cef0112f2e20b954e040d16446d
Peter Hutterer <peter.hutterer@who-t.net>
2013-01-15 09:15:33 +1000

    libXi: add test for CopyEventCookie on raw events
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- tests/lib/libXi.cpp
+++ tests/lib/libXi.cpp
@@ -13,6 +13,7 @@
 #include <device-interface.h>
 
 #include "helpers.h"
+#include "xit-event.h"
 
 /**
  * Test for libXi-related bugs. Initialises a single evdev pointer device ready
@@ -83,6 +84,71 @@ TEST_F(libXiTest, DisplayNotGarbage)
     ASSERT_EQ(dev->display, dpy);
 
     XFreeEventData(dpy, &ev.xcookie);
+}
+
+class libXiTouchTest : public libXiTest {
+public:
+    /**
+     * Initializes a standard mouse device with two wheels.
+     */
+    virtual void SetUp() {
+        SetDevice("tablets/N-Trig-MultiTouch.desc");
+        XITServerInputTest::SetUp();
+    }
+};
+
+TEST_F(libXiTouchTest, CopyRawTouchEvent)
+{
+    XORG_TESTCASE("Generate touch begin/end event\n"
+                  "Trigger CopyEventCookie through XPeekEvent.\n"
+                  "Ensure value is not garbage\n"
+                  "https://bugs.freedesktop.org/show_bug.cgi?id=59491");
+
+    ::Display *dpy = Display();
+
+    XIEventMask mask;
+    unsigned char data[XIMaskLen(XI_LASTEVENT)] = { 0 };
+
+    mask.deviceid = XIAllMasterDevices;
+    mask.mask = data;
+    mask.mask_len = sizeof(data);
+    XISetMask(mask.mask, XI_RawTouchBegin);
+    XISetMask(mask.mask, XI_RawTouchUpdate);
+    XISetMask(mask.mask, XI_RawTouchEnd);
+
+    XISelectEvents(dpy, DefaultRootWindow(dpy), &mask, 1);
+    XSync(dpy, False);
+
+    dev->Play(RECORDINGS_DIR "tablets/N-Trig-MultiTouch.touch_1_begin.events");
+    dev->Play(RECORDINGS_DIR "tablets/N-Trig-MultiTouch.touch_1_update.events");
+    dev->Play(RECORDINGS_DIR "tablets/N-Trig-MultiTouch.touch_1_end.events");
+
+    XSync(dpy, False);
+    ASSERT_GT(XPending(dpy), 0);
+
+    XEvent ev = {0};
+
+    ASSERT_TRUE(XPeekEvent(dpy, &ev));
+    ASSERT_EQ(ev.xcookie.extension, xi2_opcode);
+    ASSERT_EQ(ev.xcookie.evtype, XI_RawTouchBegin);
+    ASSERT_GT(ev.xcookie.cookie, 0U);
+    ASSERT_TRUE(XGetEventData(dpy, &ev.xcookie));
+    XFreeEventData(dpy, &ev.xcookie);
+    ASSERT_EVENT(XIRawEvent, begin, dpy, GenericEvent, xi2_opcode, XI_RawTouchBegin);
+
+    ASSERT_TRUE(XPeekEvent(dpy, &ev));
+    ASSERT_EQ(ev.xcookie.extension, xi2_opcode);
+    ASSERT_EQ(ev.xcookie.evtype, XI_RawTouchUpdate);
+    ASSERT_GT(ev.xcookie.cookie, 0U);
+    ASSERT_TRUE(XGetEventData(dpy, &ev.xcookie));
+    ASSERT_EVENT(XIRawEvent, update, dpy, GenericEvent, xi2_opcode, XI_RawTouchUpdate);
+
+    ASSERT_TRUE(XPeekEvent(dpy, &ev));
+    ASSERT_EQ(ev.xcookie.extension, xi2_opcode);
+    ASSERT_EQ(ev.xcookie.evtype, XI_RawTouchEnd);
+    ASSERT_GT(ev.xcookie.cookie, 0U);
+    ASSERT_TRUE(XGetEventData(dpy, &ev.xcookie));
+    ASSERT_EVENT(XIRawEvent, end, dpy, GenericEvent, xi2_opcode, XI_RawTouchEnd);
 }
 
 int main(int argc, char **argv) {
